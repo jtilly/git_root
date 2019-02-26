c1f6526b7404fa867f3dc54c992364ac5abb0a4f
Jan Tilly <jantilly@gmail.com>
2019-02-26 21:35:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# git_root
+Find the root of a git repository (pilfered from https://stackoverflow.com/a/53675112)
+
+```shell
+pip install git+https://github.axa.com/jan-tilly/git_root
+```
+
+```python
+from git_root import git_root
+git_root()
+```
---
+++ git_root/git_root.py
@@ -0,0 +1,23 @@
+def git_root(*args):
+    """
+    Returns the root path of the git repository (without trailing slash).
+    Any additional (and optional) arguments will be appended to the path
+    
+    Examples
+    ----------
+    > git_root()
+    /home/user/my_repo
+    
+    > git_root('dir1', 'file')
+    /home/user/my_repo/dir1/file
+    
+    > git_root('dir1/file')
+    /home/user/my_repo/dir1/file
+    """
+    import subprocess
+    import os
+
+    git_root = subprocess.Popen(
+        ['git', 'rev-parse', '--show-toplevel'], 
+        stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8')
+    return os.path.abspath(os.path.join(git_root, *args))
